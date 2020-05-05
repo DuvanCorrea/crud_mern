52716c946009eac7b9452bb9bb41fe7c844e719c
duvancorrea96 <duvancorrea96@gmail.com>
2020-05-05 13:06:00 -0500

    babel ok

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,15 @@
 module.exports = {
-
     entry: __dirname + "/src/app/index.js",
-
     output: {
-
         path: __dirname + "/src/public",
         filename: "bundle.js"
-
+    },
+    module: {
+        rules: [{
+            use: "babel-loader",
+            test: /\.js$/,
+            exclude: /node_module/
+        }]
     }
-
 
 }
